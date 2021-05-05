e2d77e20b5333402f1d7867238718b80094b2640
orel alon <orela231089@gmail.com>
2021-05-05 10:52:14 +0300

    heroku deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require('path');
 const app = express();
 
 //Connect Database
@@ -9,12 +9,22 @@ connectDB();
 //Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.json('hello Orel'));
+// app.get('/', (req, res) => res.json('hello Orel'));
 
 //Defint Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/orders', require('./routes/orders'));
+
+//Server static in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static floder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
